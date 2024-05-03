731b0e5a32a88bb09776ebc146862cc043ef9f76
junheeLee96 <dlwnsgml203@naver.com>
2024-05-03 14:42:55 +0900

    주석

--- server/server.js
+++ server/server.js
@@ -56,9 +56,7 @@ const createWorker = async () => {
 worker = createWorker();
 
 // This is an Array of RtpCapabilities
-// https://mediasoup.org/documentation/v3/mediasoup/rtp-parameters-and-capabilities/#RtpCodecCapability
 // list of media codecs supported by mediasoup ...
-// https://github.com/versatica/mediasoup/blob/v3/src/supportedRtpCapabilities.ts
 const mediaCodecs = [
   {
     kind: "audio",
